df8baafcf4608a16f1b15b69a0bcef6e2b37625a
Norio Nomura <norio.nomura@gmail.com>
2015-04-23 22:06:26 +0900

    Update Project Settings
    Add Cartfile
    github "jspahrsummers/xcconfigs" >= 0.8
    github "Quick/Quick" "master"
    github "Quick/Nimble" "master"
    
    iOS Framework

--- RxSwift/RxSwift.h
+++ RxSwift/RxSwift.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Norio Nomura. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for RxSwift.
 FOUNDATION_EXPORT double RxSwiftVersionNumber;

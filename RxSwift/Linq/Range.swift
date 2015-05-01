55888ae556980fc578b93387c63340f317fdc788
Norio Nomura <norio.nomura@gmail.com>
2015-05-01 18:47:41 +0900

    Range

--- RxSwift/Linq/Range.swift
+++ RxSwift/Linq/Range.swift
@@ -8,6 +8,41 @@
 
 import Foundation
 
+// MARK: public
+
+public extension Observable {
+    /**
+    Generates an observable sequence of integral numbers within a specified range, using the specified scheduler to send out observer messages.
+    
+    :param: start The value of the first integer in the sequence.
+    :param: count The number of sequential integers to generate.
+    
+    :returns: An observable sequence that contains a range of sequential integral numbers.
+    */
+    public static func range<T: IntegerType>(start: T, _ count: T) -> Observable<T> {
+        return _range(start, count)
+    }
+    
+    /**
+    Generates an observable sequence of integral numbers within a specified range, using the specified scheduler to send out observer messages.
+    
+    :param: start     The value of the first integer in the sequence.
+    :param: count     The number of sequential integers to generate.
+    :param: scheduler Scheduler to run the generator loop on. If not specified, defaults to Scheduler.currentThread.
+    
+    :returns: An observable sequence that contains a range of sequential integral numbers.
+    */
+    public static func range<T: IntegerType>(start: T, _ count: T, scheduler: IScheduler) -> Observable<T> {
+        return _range(start, count, scheduler: scheduler)
+    }
+}
+
+// MARK: private
+
+private func _range<T: IntegerType>(start: T, count: T, scheduler: IScheduler = SchedulerDefaults.iteration) -> Observable<T> {
+    return Range(start, count, scheduler: scheduler)
+}
+
 private final class Range<TResult: IntegerType>: Producer<TResult> {
     var start: TResult
     var count: TResult
@@ -35,7 +70,32 @@ private final class RangeSink<TResult: IntegerType>: Sink<TResult> {
     }
 
     func run() -> IDisposable? {
-        return parent.scheduler.schedule(action: invoke)
+        if let longRunning = parent.scheduler as? ISchedulerLongRunning {
+            return longRunning.scheduleLongRunning(state: 0, action: loop)
+        } else {
+            return (parent.scheduler as! Scheduler).schedule(state: 0, action: loopRec)
+        }
+    }
+    
+    func loop(var i: TResult, cancel: ICancelable) {
+        while !cancel.isDisposed && i < parent.count {
+            super.observer?.onNext(parent.start + i)
+            i = i.successor()
+        }
+        if !cancel.isDisposed {
+            super.observer?.onCompleted()
+        }
+        super.dispose()
+    }
+    
+    func loopRec(i: TResult, recurse: TResult -> ()) {
+        if i < parent.count {
+            super.observer?.onNext(parent.start + i)
+            recurse(i + 1)
+        } else {
+            super.observer?.onCompleted()
+            super.dispose()
+        }
     }
 
 }
---
+++ RxSwift/Linq/ToObservable.swift
@@ -0,0 +1,9 @@
+//
+//  ToObservable.swift
+//  RxSwift
+//
+//  Created by 野村 憲男 on 5/1/15.
+//  Copyright (c) 2015 Norio Nomura. All rights reserved.
+//
+
+import Foundation
